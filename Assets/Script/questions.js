47843f193c79c369de3e31e818f889d65b037898
Liem Nguyen <liemnguyen0111@gmail.com>
2020-04-28 21:43:28 -0700

    Updated script and added questions.js

--- Assets/Script/questions.js
+++ Assets/Script/questions.js
@@ -1,63 +1,17 @@
 let questions = [
     {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
+        question: 'What is the correct way to write an if-else statement?',
+        choices: 
+        ['If(condition){ //do something } else(condition){ //do something}',
+        'If(condition){ //do something } else{ //do something}',
+        'All the above',
+        'None of the above'],
+        answer: 1
     },
     {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
-    },
-    {
-        question: '',
-        choices: ['','','',''],
-        answer: '',
-        tips: ''
+        question: 'Given an integer i equal to 1, which of the following will make i equal 2',
+        choices: ['++i','i++','All of the above','None of the above'],
+        answer: 2
     }
- 
+  
 ]
--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -2,15 +2,16 @@
 let timer = 3
 let isClick  = false
 let isOver = false
+let correct = false
+let index = 0
 let currentScore = 0
 let highScore = 0
 const button = ['Start','Play Again']
-
-$('.second').text(timer)
+let date = Date().slice(0,33)
 
 let genBut = () =>
 {
-   
+    
 for(let i = 0; i < button.length;i++)
 {
     $('.myBtn').append($(`<button id="${button[i].replace(/\s/g, '')}" class = "btn btn-primary btn-lg btn-block">${button[i]}</button>`))
@@ -19,9 +20,9 @@ document.getElementById("PlayAgain").disabled = true
 }
 
 $(document).on('click', event =>
-{
+{   
 
-    console.log(questions)
+
     if(event.target.id === 'Start')
     {
         document.getElementById('currentScore').textContent = `${currentScore}`
@@ -35,6 +36,9 @@ $(document).on('click', event =>
         resetGame()
     }
 
+
+    checkAnswer(questions[index].choices.indexOf(event.target.textContent))
+
 })
 
 let timerStart = () =>
@@ -44,35 +48,43 @@ let timerStart = () =>
         if(timer > 0)
         {
         $('.second').text(timer--); 
+        gameStart(index)
         }
-        else
+        
+        if(timer === 0 ||index === questions.length)
         {
         isOver = true
         document.getElementById("PlayAgain").disabled = false
         document.getElementById('aside1').innerHTML = "<h3>Time out!<h3>"
         console.log(timer)
-        clearInterval(time)
-        score()
+        clearInterval(time)    
         }
 
     }, 1000)
-
-    gameStart()
     
 }
  
 
-let gameStart = () =>
+let gameStart = (index) =>
 {
-    console.log("1")
+    document.getElementById('btnAnswer1').innerHTML = questions[index].choices[0]
+    document.getElementById('btnAnswer2').innerHTML = questions[index].choices[1]
+    document.getElementById('btnAnswer3').innerHTML = questions[index].choices[2]
+    document.getElementById('btnAnswer4').innerHTML = questions[index].choices[3]
+    document.getElementById('question').innerHTML = questions[index].question
 }
 
 let resetGame = () =>
 {
-    timer = 3
-    isClick  = false
-    isOver = false
+     timer = 3
+     isClick  = false
+     isOver = false
+     correct = false
+     index = 0
+     currentScore = 0
+     highScore = 0
     document.getElementById('aside1').innerHTML = `<h3 class="time"><u>Time:</u></h3> <span class="second"></span> second(s)`
+    score()
     timerStart()
 }
 
@@ -82,8 +94,34 @@ const score = () =>
     // $('.currentScore').text(scores)
 }
 
+let checkAnswer = (str) =>
+{
+if(str === questions[index].answer && !isOver)
+    {
+        ++index
+        gameStart(index)
+        console.log("here")
+        console.log(index)
+        correct = true
+        currentScore++
+        score()
+    }
+    else
+    {
+        correct = false    
+    }
+}
+
+
 genBut()
 
+$('.second').text(timer)
 
-document.getElementById("myFooter").innerHTML +=  
-              `<p>Time: <span class="date">${Date().slice(0,33)}</span></p>`; 
+
+     setInterval(function(){ 
+               
+        document.getElementById("date").textContent = `${Date().slice(0,33)}`
+        
+     }, 1000)
+     
+
