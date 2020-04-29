f98ae22e43be44a2ec2c10a309ae2c4d8335310f
Liem Nguyen <liemnguyen0111@gmail.com>
2020-04-29 01:17:07 -0700

    Cleanup code

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,12 +1,12 @@
 /**Initialize game to default/begin state */
-let time = 75
+let time = 15
 let index = 0
 let currentScore = 0
 let highScore = 0
 let isOver = false
 let gameStart = true
 let interval
-
+document.getElementById("reset").disabled = true
 
 /** Add event listener */
 $(document).on('click', event =>
@@ -17,15 +17,14 @@ $(document).on('click', event =>
         timer()
         startGame()
         document.getElementById('currentScore').textContent = currentScore
-        // document.getElementById("start").disabled = true
     }
 
     if(event.target.id === 'reset')
     {
-        // document.getElementById("reset").disabled = true
         gameOver()
         timer()
         startGame()
+        document.getElementById('currentScore').textContent = currentScore
     }
 
     if(index < questions.length && !isOver)
@@ -66,8 +65,11 @@ $(document).on('click', event =>
 /** start the game */
 let startGame = () =>
 {
+    document.getElementById("start").disabled = true
+    document.getElementById("reset").disabled = true
     onScreenQA(index)
 }
+
 /** Question and choices show up*/
 let onScreenQA = (i) =>
 {
@@ -88,7 +90,6 @@ let results = (str) =>
             ++index
             currentScore++
             document.getElementById('currentScore').textContent = currentScore
-            console.log("correct " + currentScore)
             setHighScore()
             if(index < questions.length)
             {
@@ -97,6 +98,7 @@ let results = (str) =>
         }
         else
         {
+            console.log(isOver)
             console.log('it goes here instead')
             time -= 2
         }
@@ -105,14 +107,13 @@ let results = (str) =>
         let temp = questions.length
         if(index === temp)
         {
-            gameOver()
+            document.getElementById('question').innerHTML = `Congratulation ! You made it through, press "Try Again" button to play again`
+            isOver = true
+            console.log(isOver)
+            document.getElementById("reset").disabled = false
         }
 }
-/** Play again and reset game and  */
-let playAgain = () =>
-{
 
-}
 /** Store high score into client-side storage */
 let setHighScore = () =>
 {
@@ -141,15 +142,29 @@ const getHighScore = () =>
 /** If game is over, set scene to display play again button*/
 let gameOver = () =>
 {
-    //Initalize to default to start new game
-    console.log("finish")
-    clearInterval(interval)
-     time = 75
-     index = 0
-     currentScore = 0
-     isOver = false
-     gameStart = true
-     highScore = getHighScore()
+     // Check if game is over and not answered all questions
+     if(isOver && index < questions.length)
+                {
+                    if (index > 1)
+                    {
+                    document.getElementById('question').innerHTML = `Good job ! you made it through ${currentScore} question(s). 
+                                                                        Wanna try again? Press the "Play Again" button above.`
+                    }
+                    else
+                    {  
+                        document.getElementById('question').innerHTML = `Uh oh... ! Please press the "Try Again" button above to play again`
+                    }
+}
+
+            //Initalize to default to start new game
+            clearInterval(interval)
+            time = 15
+            index = 0
+            currentScore = 0
+            isOver = false
+            gameStart = true
+            document.getElementById("reset").disabled = false    
+            highScore = getHighScore()
 }
 
 /** Timer*/
@@ -163,13 +178,16 @@ let timer = () =>
                 else
                 {
                     isOver = true
+                    gameOver()
                     clearInterval(interval)
                 }  
                 if(index === questions.length)
                 {
                     clearInterval(interval)
                 }   
+
                 }, 1000)
+     
 }
 
 /** Get data from client-side storage and store it into highscore */
@@ -182,131 +200,3 @@ setInterval(function(){
             document.getElementById("date").textContent = `${Date().slice(0,33)}`
             
          }, 1000)
-
-
-// let timer = 3
-// let isClick  = false
-// let isOver = false
-// let correct = false
-// let index = 0
-// let currentScore = 00
-// let highScore = 0
-// const button = ['Start','Play Again']
-// let date = Date().slice(0,33)
-
-// let genBut = () =>
-// {
-    
-// for(let i = 0; i < button.length;i++)
-// {
-//     $('.myBtn').append($(`<button id="${button[i].replace(/\s/g, '')}" class = "btn btn-primary btn-lg btn-block">${button[i]}</button>`))
-// }
-// document.getElementById("PlayAgain").disabled = true
-// }
-
-// $(document).on('click', event =>
-// {   
-
-
-//     if(event.target.id === 'Start')
-//     {
-//         document.getElementById('currentScore').textContent = `${currentScore}`
-//         document.getElementById("Start").disabled = true
-//         timerStart()
-//     }
-
-//     if(event.target.id === 'PlayAgain')
-//     {
-//         document.getElementById("PlayAgain").disabled = true
-//         resetGame()
-//     }
-
-
-//     checkAnswer(questions[index].choices.indexOf(event.target.textContent))
-
-// })
-
-// let timerStart = () =>
-// {
-   
-//    let time = setInterval(function(){ 
-//         if(timer > 0)
-//         {
-//         $('.second').text(timer--); 
-//         gameStart(index)
-//         }
-        
-//         if(timer === 0 ||index === questions.length)
-//         {
-//         isOver = true
-//         document.getElementById("PlayAgain").disabled = false
-//         document.getElementById('aside1').innerHTML = "<h3>Time out!<h3>"
-//         console.log(timer)
-//         clearInterval(time)    
-//         }
-
-//     }, 1000)
-    
-// }
- 
-
-// let gameStart = (index) =>
-// {
-//     document.getElementById('btnAnswer1').innerHTML = questions[index].choices[0]
-//     document.getElementById('btnAnswer2').innerHTML = questions[index].choices[1]
-//     document.getElementById('btnAnswer3').innerHTML = questions[index].choices[2]
-//     document.getElementById('btnAnswer4').innerHTML = questions[index].choices[3]
-//     document.getElementById('question').innerHTML = questions[index].question
-// }
-
-// let resetGame = () =>
-// {
-//      timer = 3
-//      isClick  = false
-//      isOver = false
-//      correct = false
-//      index = 0
-//      currentScore = 0
-//      highScore = 0
-//     document.getElementById('aside1').innerHTML = `<h3 class="time"><u>Time:</u></h3> <span class="second"></span> second(s)`
-//     score()
-//     timerStart()
-// }
-
-// const score = () =>
-// {
-//     document.getElementById('currentScore').textContent = `${currentScore}`
-//     // $('.currentScore').text(scores)
-// }
-
-// let checkAnswer = (str) =>
-// {
-// if(str === questions[index].answer && !isOver)
-//     {
-//         ++index
-//         gameStart(index)
-//         console.log("here")
-//         console.log(index)
-//         correct = true
-//         currentScore++
-//         score()
-//     }
-//     else
-//     {
-//         correct = false    
-//     }
-// }
-
-
-// genBut()
-
-// $('.second').text(timer)
-
-
-//      setInterval(function(){ 
-               
-//         document.getElementById("date").textContent = `${Date().slice(0,33)}`
-        
-//      }, 1000)
-     
-
